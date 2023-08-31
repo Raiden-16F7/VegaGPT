21c2f5e0e1e3a02796974a52aa0ef7f94dc720dc
Raiden-16F7 <fahad.rizwa07@gmail.com>
2023-08-31 20:14:47 +0500

    MainScreen Design

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import LottieView from "lottie-react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<SafeAreaView style={styles.container}>
+			<View
+				style={{
+					marginTop: 20,
+					alignItems: "center",
+					justifyContent: "center",
+				}}>
+				<LottieView
+					autoPlay
+					style={{
+						width: 200,
+						height: 200,
+					}}
+					// Find more Lottie files at https://lottiefiles.com/featured
+					source={require("./assets/animation_llza5dm0.json")}
+				/>
+				<Text style={{ fontSize: 30, fontWeight: "bold" }}>✵VegaGPT</Text>
+			</View>
+			<StatusBar style="auto" />
+		</SafeAreaView>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		padding: 20,
+	},
 });
